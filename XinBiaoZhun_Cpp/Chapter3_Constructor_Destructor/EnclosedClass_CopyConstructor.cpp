b2e9f791782bfa1f941cb41a12b2e2408af58b55
ltimaginea <ltimaginea@163.com>
2020-09-02 23:31:02 +0800

    EnclosedClass_CopyConstructor

---
+++ XinBiaoZhun_Cpp/Chapter3_Constructor_Destructor/EnclosedClass_CopyConstructor.cpp
@@ -0,0 +1,32 @@
+﻿// 封闭类的复制构造函数
+
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	A()
+	{
+		cout << "default" << endl;
+	}
+	A(const A&)
+	{
+		cout << "copy" << endl;
+	}
+};
+class B
+{
+private:
+	A a;
+};
+
+int main()
+{
+	B b1, b2(b1);			// 程序先后输出：default  copy
+	return 0;
+}
+
+// tips: 
+//   1. 封闭类的对象，如果是用默认复制构造函数初始化的，那么它包含的成员对象也会用复制构造函数初始化。
