c3275b33424ae4d4379e1eb0eb3ab2e28e7b5036
ltimaginea <ltimaginea@163.com>
2020-09-06 20:17:40 +0800

    friend

--- XinBiaoZhun_Cpp/Chapter3_Constructor_Destructor/Friend1_Function.cpp
+++ XinBiaoZhun_Cpp/Chapter3_Constructor_Destructor/Friend1_Function.cpp
@@ -9,13 +9,16 @@ class CDriver
 {
 public:
 	void ModifydCar(CCar* pCar);
+private:
+	void PrintInfo();
 };
 class CCar
 {
 private:
 	int price;
-	friend void CDriver::ModifydCar(CCar* pCar);				// 声明友元
 	friend int MostExpensiveCar(CCar cars[], int total);			// 声明友元
+	friend void CDriver::ModifydCar(CCar* pCar);				// 声明友元
+	//friend void CDriver::PrintInfo();					// 错误！不能把其他类的私有成员函数声明为友元，编译时会提示“无法访问该私有成员”
 };
 void CDriver::ModifydCar(CCar* pCar)
 {
@@ -40,7 +43,7 @@ int main()
 // tips: 
 //   1. 友元分为友元函数和友元类。
 //   2. 定义一个类时，可以把一些函数(包括全局函数和其他类的成员函数)声明为“友元”(友元函数)，这样在友元函数内部就可以访问该类对象的私有成员。
-//   3. 注意，不能把其他类的私有成员函数声明为友元。
+//   3. 注意，不能把其他类的私有成员函数声明为友元。编译时会提示“无法访问该私有成员”。
 //   4. 友元关系不能传递。
 //   5. 可以简单地将一个类的名字提前声明，以便在该类的定义出现之前使用使用该类，如第7行的提前声明和第11行的使用。
 //      尽管可以提前声明，但是在一个类的定义出现之前，仍然不能有任何导致该类对象被生成的语句。但使用该类的指针或引用是没有问题的。
