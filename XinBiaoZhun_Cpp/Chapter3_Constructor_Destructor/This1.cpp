07f35a2a87f98c6baf909b50b0c5a052b6e382fa
ltimaginea <ltimaginea@163.com>
2020-09-17 22:29:53 +0800

    this_1

---
+++ XinBiaoZhun_Cpp/Chapter3_Constructor_Destructor/This1.cpp
@@ -0,0 +1,31 @@
+﻿// this 指针
+
+#include <iostream>
+
+using namespace std;
+
+class CComplex
+{
+public:
+	double real;
+	double imag;
+	CComplex(double real_, double imag_) :real(real_), imag(imag_) {}
+	CComplex AddOne();
+};
+CComplex CComplex::AddOne()
+{
+	++this->real;		// 等效于 ++real
+	return *this;
+}
+
+int main()
+{
+	CComplex a(10, 5);
+	CComplex b = a.AddOne();
+	cout << "real=" << b.real << ",imag=" << b.imag << endl;	// 输出 real=11,imag=5
+	return 0;
+}
+
+// tips: 
+//   1. this 指针， this 代表指向成员函数所作用对象的指针， *this 即代表成员函数所作用的对象。
+//   2. 因为静态成员函数并作用于某个对象，所以在其内部不能使用 this 指针。
