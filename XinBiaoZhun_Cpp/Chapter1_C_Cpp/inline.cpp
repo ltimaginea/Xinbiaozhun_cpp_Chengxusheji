f290871a1c1903dcef80330b22bee755a48c4902
ltimaginea <ltimaginea@163.com>
2020-09-19 23:27:59 +0800

    inline

---
+++ XinBiaoZhun_Cpp/Chapter1_C_Cpp/inline.cpp
@@ -0,0 +1,19 @@
+﻿// inline
+
+#include <iostream>
+
+using namespace std;
+
+inline int Max(int a, int b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
+int main()
+{
+	cout << Max(23, 5) << endl;
+	return 0;
+}
