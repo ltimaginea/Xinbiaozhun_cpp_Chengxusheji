cc23714ac6cd3dc80060c1e75881d58039c10dbd
ltimaginea <ltimaginea@163.com>
2020-10-04 10:17:58 +0800

    << >>

--- XinBiaoZhun_Cpp/Chapter4_Overload/OperatorOverloading6.cpp
+++ XinBiaoZhun_Cpp/Chapter4_Overload/OperatorOverloading6.cpp
@@ -48,4 +48,4 @@ int main()
 //   1. cout是ostream类的对象。ostream类和cout都是在头文件iostream中声明的。ostream类将"<<"重载为成员函数，而且重载了多次，其中实现了“输出”功能。cin同理。
 //   2. 因为没有办法修改ostream类和istream类，所以只能将"<<"和">>"重载为全局函数的形式。
 //   3. operator<< 函数的参数 os 只能是ostream的引用，而不能是ostream对象，因为ostream的复制构造函数是私有的，没有办法生成ostream参数对象。operator>> 同理。
-//   4. operator<< 函数的返回值类型设为 ostream& ，是为了实现"<<"的连续使用，是保持运算符的原有特性的做法。operator>> 同理。
+//   4. operator<< 函数的返回值类型设为 ostream& ，也是为了实现"<<"的连续使用，是保持运算符的原有特性的做法。operator>> 同理。
