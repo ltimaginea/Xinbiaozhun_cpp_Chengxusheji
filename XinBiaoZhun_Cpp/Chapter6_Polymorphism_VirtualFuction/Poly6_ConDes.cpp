468e1ef5579d8938dc9af8f9eff14256317cefb3
ltimaginea <ltimaginea@163.com>
2020-12-10 23:59:33 +0800

    构造函数和析构函数中调用虚函数

---
+++ XinBiaoZhun_Cpp/Chapter6_Polymorphism_VirtualFuction/Poly6_ConDes.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	virtual void hello() { cout << "A::hello" << endl; }
+	virtual void bye() { cout << "A::bye" << endl; }
+};
+
+class B :public A
+{
+public:
+	virtual void hello() { cout << "B::hello" << endl; }
+	B() { hello(); }
+	~B() { bye(); }
+};
+
+class C :public B
+{
+public:
+	virtual void hello() { cout << "C::hello" << endl; }
+};
+
+int main()
+{
+	C obj;
+	return 0;			// 输出 B::hello		A::bye
+}
+
+
+
+// tips:
+//	1. 在构造函数和析构函数中调用虚函数不是多态。
