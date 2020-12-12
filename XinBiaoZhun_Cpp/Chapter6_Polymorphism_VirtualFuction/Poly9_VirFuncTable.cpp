3ec6b241d2ebd062e8c3fa43912eac449a0e490a
ltimaginea <ltimaginea@163.com>
2020-12-12 23:07:50 +0800

    虚函数表

---
+++ XinBiaoZhun_Cpp/Chapter6_Polymorphism_VirtualFuction/Poly9_VirFuncTable.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class CBase
+{
+public:
+	int i;
+	void func1() {}
+};
+
+class CDerived :public CBase
+{
+public:
+	virtual void func2() {}
+};
+
+class CDerived2 :public CDerived
+{
+	void func3() {}
+};
+
+int main()
+{
+	cout << sizeof(CBase) << "," << sizeof(CDerived) << "," << sizeof(CDerived2) << endl;	// 输出 4,8,8
+	return 0;
+}
+
+
+
+// tips:
+//	1. 可以发现，有了虚函数以后，对象所占用的存储空间比没有虚函数时多了4个字节。这4个字节就是实现多态的关键——它位于对象存储空间的最前端，其中存放的是虚函数表的地址。
+//	2. 每一个有虚函数的类（或有虚函数的类的派生类）都有一个虚函数表，该类的任何对象中都放着该虚函数表的指针（可以认为这是由编译器自动添加到构造函数中的指令完成的）。
+//	虚函数表是编译器生成的，程序运行时被载入内存。一个类的虚函数表中列出了该类的全部虚函数地址。
