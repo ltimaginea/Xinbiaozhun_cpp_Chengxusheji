d5821574a2d33fc19743a127938bc5f619d3a8cf
ltimaginea <ltimaginea@163.com>
2021-01-02 23:59:44 +0800

    普通(非模板)类的成员模板

---
+++ XinBiaoZhun_Cpp/Chapter9_GenericProgram/Gen6_MemberTemplate1.cpp
@@ -0,0 +1,31 @@
+﻿// 普通（非模板）类的成员模板
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	template <typename T2>
+	void func(T2 t);
+};
+
+template <typename T2>
+void A::func(T2 t)
+{
+	cout << t << endl;
+}
+
+int main()
+{
+	A a;
+	a.func("hello");
+	a.func(2021);
+	return 0;
+}
+
+
+
+// tips:
+//	1. 一个类（无论是普通类还是类模板）可以包含本身是模板的成员函数。这种成员被称为成员模板（member template）。
+//	2. 成员模板不能是虚函数。
