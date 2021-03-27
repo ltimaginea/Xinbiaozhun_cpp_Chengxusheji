07230cba858d0174a316cc1cbfb037c3fd7cbd58
ltimaginea <ltimaginea@163.com>
2021-03-27 21:43:00 +0800

    lambda expression

---
+++ XinBiaoZhun_Cpp/Chapter11_AdvancedTopics/Ch11_03_lambda_00.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int a[4] = { 2,11,33,4 };
+	sort(begin(a), end(a), [](int i, int j)->bool { return i % 10 > j % 10; });
+	for_each(begin(a), end(a), [](int i) { cout << i << " "; });
+	cout << endl;
+	int total = 0;
+	for_each(begin(a), end(a), [&](int& i)->void { total += i; i *= 2; });
+	cout << total << endl;
+	for_each(begin(a), end(a), [](int i) { cout << i << " "; });
+	cout << endl;
+
+	return 0;
+}
+
+// tips: 
+//   1. Lambda 表达式实际上是一个匿名函数。
---
+++ XinBiaoZhun_Cpp/Chapter11_AdvancedTopics/Ch11_03_lambda_01.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 100, y = 200, z = 300;
+	auto ff = [=, &y, &z](int n)->int
+	{
+		cout << x << endl;
+		++y;
+		++z;
+		return n * n;
+	};
+	cout << ff(15) << endl;
+	cout << y << "," << z << endl;
+
+	return 0;
+}
+
+// tips: 
+//   1. Lambda 表达式实际上是一个匿名函数。
