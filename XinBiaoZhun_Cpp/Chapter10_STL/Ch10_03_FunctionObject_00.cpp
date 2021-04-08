50002a9e7a5fe0be157ddc191b5027f48c44c234
ltimaginea <ltimaginea@163.com>
2021-04-08 23:18:47 +0800

    FunctionObject

---
+++ XinBiaoZhun_Cpp/Chapter10_STL/Ch10_03_FunctionObject_00.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Average
+{
+public:
+	double operator()(int a1, int a2, int a3)
+	{
+		return (double)(a1 + a2 + a3) / 3;
+	}
+};
+
+int main()
+{
+	Average average;
+	cout << average(3, 2, 3) << endl;			// 等价于 average.operator()(3, 2, 3)
+	cout << average.operator()(3, 2, 3) << endl;
+	cout << Average()(1, 2, 3) << endl;			// 生成一个临时匿名函数对象
+	return 0;
+}
