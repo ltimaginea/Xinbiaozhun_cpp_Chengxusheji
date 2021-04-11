ad78d18df2411661f5f1ce47fd043c1d75c51277
ltimaginea <ltimaginea@163.com>
2021-04-11 22:44:04 +0800

    FunctionObject

---
+++ XinBiaoZhun_Cpp/Chapter10_STL/Ch10_03_FunctionObject_01.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <array>
+#include <numeric>
+
+using namespace std;
+
+template <typename T>
+void PrintInterval(T first, T last)
+{
+	for (; first != last; ++first)
+	{
+		cout << *first << " ";
+	}
+	cout << endl;
+}
+
+int SumSquares(int total, int value)
+{
+	return total + value * value;
+}
+
+template <typename T>
+class SumPowers
+{
+private:
+	int power_;
+public:
+	SumPowers(int power) :power_(power) {  }
+	const T operator()(const T& total, const T& value)
+	{
+		T v = value;
+		for (int i = 0; i < power_ - 1; ++i)
+		{
+			v = v * value;
+		}
+		return total + v;
+	}
+};
+
+int main()
+{
+	const int kSize = 10;
+	array<int, kSize> a = { 1,2,3,4,5,6,7,8,9,10 };
+	array<double, kSize> d = { 1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9,10.17 };
+	PrintInterval(a.begin(), a.end());
+	PrintInterval(d.begin(), d.end());
+	auto result1 = accumulate(a.begin(), a.end(), 0, SumSquares);				// 函数指针
+	cout << result1 << endl;
+	auto result2 = accumulate(a.begin(), a.end(), 0, SumPowers<int>(3));		// 会生成一个模板类的临时对象
+	cout << result2 << endl;
+	auto result3 = accumulate(d.begin(), d.end(), static_cast<double>(0), SumPowers<double>(4));
+	cout << result3 << endl;
+
+	return 0;
+}
